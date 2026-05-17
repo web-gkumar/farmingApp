5d4db1f84d0ab3dc4f74d46296225cccb596782a
web-gkumar <kgautam431@gmail.com>
2026-05-17 20:16:57 +0530

    profile page fixing

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CarouselModule, MatCardModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+
+  customOptions: OwlOptions = {
+    loop: true,
+    dots: true,
+    autoplay: true,
+    nav: false,
+    responsive: {
+      0: {items: 1},
+      400: {items: 2},
+      740: {items: 3},
+      940: {items: 4}
+    },
+    
+  }
+
+  crops: any[] = [];
+  
+ngOnInit(): void {
+  this.crops = localStorage.getItem('my-orders') ? JSON.parse(localStorage.getItem('my-orders')!) : [];
+}
+
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // baseUrl: 'https://new-backend-w7jv.onrender.com/api'
-  baseUrl: 'http://localhost:5000/api'
+   baseUrl: 'https://new-backend-w7jv.onrender.com/api'
+  // baseUrl: 'http://localhost:5000/api'
 };
